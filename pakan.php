ebf9ff38e569de9d9e52833f03e6151e0c661463
marisasetya <marisasetya11@gmail.com>
2022-12-02 10:48:07 +0700

    title

--- pakan.php
+++ pakan.php
@@ -23,7 +23,7 @@ $sesLvl = $_SESSION['level'];
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>PHITIX</title>
 
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
