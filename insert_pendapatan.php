97a17c8d093f267a3fdc26d2359b273fb136f5ba
Aishanda Vania Aanisah <e31210681@student.polije.ac.id>
2022-12-10 12:40:35 +0700

    css

--- insert_pendapatan.php
+++ insert_pendapatan.php
@@ -56,7 +56,7 @@ if(isset($_POST['insert_data'])) {
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
